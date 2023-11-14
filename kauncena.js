f032233359b684dcf3c99f03ed84881d58492e40
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-11-14 14:30:14 -0800

    Update kauncena.js

--- kauncena.js
+++ kauncena.js
@@ -1,1 +1,27 @@
-
+function cbypass_v(text) {
+    var out = "";
+    var words = [
+        "damn",
+        "ass",
+        "shit",
+        "bastard",
+        "fuck"
+    ];
+    for(var i = 0; i < text.length; i++) {
+        var cuss = false;
+        var dd = text.toLowerCase();
+        for(var k = 0; k < words.length; k++) {
+            if(dd[i]==words[k][0]) {
+                if(dd.slice(i+1,i+words[k].length)==words[k].slice(1)) {
+                    cuss = true
+                };
+            };
+        };
+        if(cuss) {
+            out+=text[i]+"\u200e";
+        } else {
+            out+=text[i];
+        };
+    };
+    return out;
+};
