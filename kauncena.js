6632da3886533645e3661b597a7959f0e8090508
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-11-14 15:00:25 -0800

    Update kauncena.js

--- kauncena.js
+++ kauncena.js
@@ -1,23 +1,42 @@
 function cbypass_v(text) {
     var out = "";
     var words = [
+        /* uncensor death */
+        "kill",
+        "die",
+        "death",
+        "murder",
+        /* uncensor mild sexual content */
+        "sex",
+        "s&m",
+        "ligma",
+        "deez",
+        "ball",
+        "kiss",
+        /* uncensor vulgar words */
+        "crap",
+        "piss",
+        "frick",
+        "lmao",
+        "wtf",
+        "baka",
+        "kuso",
+        /* uncensor mild profanity */
         "damn",
-        "ass",
-        "shit",
-        "bastard",
-        "fuck"
+        "dammit",
+        "ass"
     ];
     for(var i = 0; i < text.length; i++) {
-        var cuss = false;
+        var Allow = false;
         var dd = text.toLowerCase();
         for(var k = 0; k < words.length; k++) {
             if(dd[i]==words[k][0]) {
                 if(dd.slice(i+1,i+words[k].length)==words[k].slice(1)) {
-                    cuss = true
+                    Allow = true
                 };
             };
         };
-        if(cuss) {
+        if(Allow) {
             out+=text[i]+"\u200e";
         } else {
             out+=text[i];
